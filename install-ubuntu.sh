d7a2164ef3d47f69954b0f0e73d86b242a91a50a
valerie16901 <valerie_16901@hotmail.com>
2024-11-22 20:40:29 +0100

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -11,14 +11,6 @@ ufw allow 9735/tcp
 # Enable the firewall
 yes | ufw enable
 
-#Install docker
-apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-apt-cache policy docker-ce
-apt install docker-ce
-
 #Install BTCPayServer
 cd # ensure we are in root home
 git clone https://github.com/btcpayserver/btcpayserver-docker
